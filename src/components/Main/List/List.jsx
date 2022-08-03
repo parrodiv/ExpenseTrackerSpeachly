6c221d5a0973e975246b92c32d1acf75da880114
Alessandro Parrilla <paggio96@gmail.com>
2022-08-03 10:50:09 +0200

    List transactions style completed

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,86 @@
+import React, { useContext } from 'react'
+import {
+  List as MUIList,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  ListItemSecondaryAction,
+  IconButton,
+  Slide,
+} from '@material-ui/core'
+import { Delete, MoneyOff } from '@material-ui/icons'
+
+import { ExpenseTrackerContext } from '../../../context/context'
+
+import useStyles from './styles'
+
+const List = () => {
+  const classes = useStyles()
+  const globalState = useContext(ExpenseTrackerContext)
+
+  console.log(globalState);
+
+  const transactions = [
+    {
+      id: 1,
+      type: 'Income',
+      category: 'Salary',
+      amount: 50,
+      date: 'Tue Aug 02 2022',
+    },
+    {
+      id: 2,
+      type: 'Expense',
+      category: 'Weed',
+      amount: 50,
+      date: 'Tue Aug 02 2022',
+    },
+    {
+      id: 3,
+      type: 'Income',
+      category: 'Salary',
+      amount: 50,
+      date: 'Tue Aug 02 2022',
+    },
+  ]
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        <Slide
+          direction='down'
+          in
+          mountOnEnter
+          unmountOnExit
+          key={transaction.id}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar
+                className={
+                  transaction.type === 'Income'
+                    ? classes.avatarIncome
+                    : classes.avatarExpense
+                }
+              >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={transaction.category}
+              secondary={`${transaction.amount} - ${transaction.date}`}
+            />
+            <ListItemSecondaryAction>
+              <IconButton edge='end' aria-label='delete'>
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  )
+}
+
+export default List
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
 import Form from './Form/Form';
+import List from './List/List'
 
 import useStyles from './styles';
+
 
 const Main = () => {
   const classes = useStyles()
@@ -22,7 +24,7 @@ const Main = () => {
       <CardContent className={classes.cardContent}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            {/* <List /> */}
+           <List />
           </Grid>
         </Grid>
       </CardContent>
