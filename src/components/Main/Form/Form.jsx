3426968b6cc629e3cdf6447065599232dae3ea45
Alessandro Parrilla <paggio96@gmail.com>
2022-08-05 11:33:45 +0200

    Context reducer settled and formData on Form.jsx component

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -2,43 +2,80 @@ import React from 'react'
 import { Grid, TextField, Typography, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
 
 import useStyles from './styles'
+import { useState } from 'react'
+
+const initialState = {
+  amount: '',
+  category: '',
+  type: 'Income',
+  date: new Date()
+}
 
 const Form = () => {
   const classes = useStyles();
+  const [formData, setFormData] = useState(initialState)
+
+  console.log(formData);
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Typography align="center" variant="subtitle2" gutterBottom>
+        <Typography align='center' variant='subtitle2' gutterBottom>
           ...
         </Typography>
       </Grid>
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Type</InputLabel>
-          <Select>
-            <MenuItem value="Income">Income</MenuItem>
-            <MenuItem value="Expense">Expense</MenuItem>
+          <Select
+            value={formData.type}
+            onChange={(e) => setFormData({ ...formData, type: e.target.value })}
+          >
+            <MenuItem value='Income'>Income</MenuItem>
+            <MenuItem value='Expense'>Expense</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Category</InputLabel>
-          <Select>
-            <MenuItem value="business">Business</MenuItem>
-            <MenuItem value="salary">Salary</MenuItem>
+          <Select
+            value={formData.category}
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
+            <MenuItem value='business'>Business</MenuItem>
+            <MenuItem value='salary'>Salary</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type="number" label="Amount" fullWidth />
+        <TextField
+          type='number'
+          label='Amount'
+          fullWidth
+          value={formData.amount}
+          onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
+        />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth />
+        <TextField
+          type='date'
+          label='Date'
+          fullWidth
+          value={formData.date}
+          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+        />
       </Grid>
-      <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
-      
+      <Button
+        className={classes.button}
+        variant='outlined'
+        color='primary'
+        fullWidth
+      >
+        Create
+      </Button>
     </Grid>
   )
 }
--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,7 +1,19 @@
 // Reducer => is a function that takes in the old state and an action  => new state...
 
 const contextReducer = (state, action) => {
+  let transactions
 
+  switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      transactions = state.filter((t) => t.id !== action.payload)
+      return transactions;
+    case 'ADD_TRANSACTION':
+      transactions = [action.payload, ...state]
+      return transactions
+      break
+    default:
+      return state
+  }
 }
 
 export default contextReducer
