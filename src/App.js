1a86c22c41f6a0a28ffa691a83101a8b3b5278fa
Alessandro Parrilla <paggio96@gmail.com>
2022-08-21 23:08:16 +0200

    Mobile view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Grid } from '@material-ui/core'
 import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui'
 
@@ -6,23 +6,34 @@ import Details from './components/Details/Details'
 import Main from './components/Main/Main'
 import useStyles from './styles'
 
+
 const App = () => {
   const classes = useStyles()
 
   return (
     <div>
-      <Grid className={classes.grid}  container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh'}}>
-        <Grid item xs={12} sm={4}>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems='center'
+        justifyContent='center'
+        style={{ height: '100vh' }}
+      >
+        <Grid item xs={12} sm={3} className={classes.mobile}>
           <Details title={'Income'} />
         </Grid>
-        <Grid item xs={12} sm={3}>
-          <Main /> 
+        <Grid item xs={12} sm={3} className={classes.main}>
+          <Main />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={3} className={classes.desktop}>
+          <Details title={'Income'} />
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.last}>
           <Details title={'Expense'} />
         </Grid>
       </Grid>
-      <PushToTalkButtonContainer>
+      <PushToTalkButtonContainer >
         <PushToTalkButton />
       </PushToTalkButtonContainer>
     </div>
